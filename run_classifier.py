1382adc58cbe3afbe36a8f43c72ad86c64ab5896
Sun Yitao <sunyitao8@gmail.com>
2019-03-18 17:59:37 +0800

    fix processors

--- run_classifier.py
+++ run_classifier.py
@@ -238,7 +238,7 @@ class FashionProcessor(DataProcessor):
       guid = "%s-%s" % (set_type, i)
       if set_type == "test":
         text_a = tokenization.convert_to_unicode(line[1])
-        label = "0"
+        label = "17"
       else:
         text_a = tokenization.convert_to_unicode(line[3])
         label = tokenization.convert_to_unicode(line[1])
@@ -325,7 +325,7 @@ class MobileProcessor(DataProcessor):
       guid = "%s-%s" % (set_type, i)
       if set_type == "test":
         text_a = tokenization.convert_to_unicode(line[1])
-        label = "0"
+        label = "31"
       else:
         text_a = tokenization.convert_to_unicode(line[3])
         label = tokenization.convert_to_unicode(line[1])
